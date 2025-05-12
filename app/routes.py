8376a7a51eb5da952859b6cbe9994b76859caff8
BorodinRoman <borodinroman966@gmail.com>
2025-05-13 00:54:29 +1000

    add agent add page

--- app/routes.py
+++ app/routes.py
@@ -8,3 +8,24 @@ from . import app
 def home():
     agents_list = Agent.query.all()
     return render_template("index.html", agents_list=agents_list)
+
+@app.route("/add", methods=["GET", "POST"])
+def add_agent():
+    if request.method == "POST":
+        agent_data = {
+            "name": request.form["name"],
+            "number": request.form["number"],
+            "email": request.form["email"],
+            "level": request.form["level"]
+        }
+
+        for k, v in agent_data.items():
+            if not v.strip():
+                return redirect(url_for('home'))
+
+        new_agent = Agent(**agent_data)
+        db.session.add(new_agent)
+        db.session.commit()
+        return redirect(url_for('home'))
+
+    return render_template("add_agent.html")
