1c6e5e790425b79551dde95a9887bf6a00503e00
BorodinRoman <borodinroman966@gmail.com>
2025-05-13 01:07:58 +1000

    add agent edit page

--- app/routes.py
+++ app/routes.py
@@ -38,3 +38,22 @@ def get_agent(id):
     return render_template("get_agent.html", agent=agent)
 
 
+@app.route("/edit/<int:id>", methods=["GET", "POST"])
+def edit_agent(id):
+    agent = Agent.query.get_or_404(id)
+    if request.method == "POST":
+        raw_info = {
+            "name": request.form["name"],
+            "number": request.form["number"],
+            "email": request.form["email"],
+            "level": request.form["level"]
+        }
+
+        new_info = {key: value for key, value in raw_info.items() if value and value.strip()}
+        for k, v in new_info.items():
+            setattr(agent, k, v)
+
+        db.session.commit()
+        return redirect(url_for('home'))
+
+    return render_template("edit_agent.html", agent=agent)
